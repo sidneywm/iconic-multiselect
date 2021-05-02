d07fe5a041658ac0d999397caa955a503214bb31
Sidney WIMART <sidney.wimart@ncit.nc>
2021-05-02 21:46:41 +1100

    update version

--- src/multi-select.js
+++ src/multi-select.js
@@ -1,11 +1,11 @@
 /*!
- * IconicMultiSelect v0.4.1
+ * IconicMultiSelect v0.4.0
  * Licence:  MIT
  * (c) 2021 Sidney Wimart.
  */
 
 /**
- * @version IconicMultiSelect v0.4.1
+ * @version IconicMultiSelect v0.4.0
  * @licence  MIT
  */
 class IconicMultiSelect {
